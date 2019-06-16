80e22de4f60414717c1c80c0a35828879fedd7fa
Jason <jasondavis707@yahoo.com.au>
2019-06-16 20:23:39 +1000

    my first commit for Twit

---
+++ bot.js
@@ -0,0 +1,38 @@
+console.log('The Bot is starting');
+
+var Twit = require('twit');
+
+var config = require('./config');
+
+console.log(config);
+
+var T = new Twit(config);
+
+
+
+setInterval(tweetATweet, 1800000);
+//
+//  tweet 'hello again...'
+//
+function tweetATweet() {
+T.post('statuses/update', { status: 'Hello from node bot.js running on Heroku...with a Random number of ' + getRandomInt(99) }, function(err, data, response) {
+  console.log(data)
+  
+})
+
+}
+
+
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+/*
+// this does what it looks like it would do
+T.get('search/tweets', { q: 'kurtcobain since:2011-07-11', count: 100 }, function(err, data, response) {
+  console.log(data)
+})
+*/
---
+++ config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+	
+	consumer_key:	'TRI7KQsNhaozs5lMJxqlLYEKn',
+	consumer_secret: 	'C3y7UCMRDRqKfOiM1lWUBxhLwTM2g7573Gxj9icKRGHLlPoZ8a',
+	access_token:	'3277685384-Sq83HREs1rCNjimHuL5nhKFpu9Mu5vXvMfYL64u',
+	access_token_secret:	'Z8iFXXlm3SeQxiYEKRWB35krUtjUeOjOIO2ZsDhfTXFaS'
+	
+	
+}
